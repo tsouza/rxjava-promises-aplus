082bc19e5e8094dae07326c290bacf48f73ff5d2
Thiago Souza <tcostasouza@gmail.com>
2015-02-09 16:21:02 -0200

    make Value extensible

--- rxjava-promises-api/src/main/java/com/github/tsouza/promises/Value.java
+++ rxjava-promises-api/src/main/java/com/github/tsouza/promises/Value.java
@@ -23,11 +23,11 @@
  */
 package com.github.tsouza.promises;
 
-public final class Value<R> implements PromiseOrValue<R> {
+public class Value<R> implements PromiseOrValue<R> {
 	
 	private final R value;
 
-	Value(R value) {
+	public Value(R value) {
 		this.value = value;
 	}
 
